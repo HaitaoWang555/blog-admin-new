e6ccaa78c723da212a06fa4f2365cd82bf59b3f9
wanghaitao <15124505701@163.com>
2020-06-17 07:35:13 +0800

    chore: style

--- vue.config.js
+++ vue.config.js
@@ -139,6 +139,11 @@ module.exports = {
                   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
                   test: /[\\/]node_modules[\\/]_?tui-editor(.*)/ // in order to adapt to cnpm
                 },
+                codemirror: {
+                  name: 'chunk-tuiEditor', // split elementUI into a single package
+                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                  test: /[\\/]node_modules[\\/]_?tcodemirror(.*)/ // in order to adapt to cnpm
+                },
                 highlight: {
                   name: 'chunk-highlight', // split elementUI into a single package
                   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
