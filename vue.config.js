2a57993a0d8a60a51389da1653030ecfe4cae2ab
wanghaitao <15124505701@163.com>
2020-06-17 07:41:29 +0800

    fix: codemirror

--- vue.config.js
+++ vue.config.js
@@ -142,7 +142,7 @@ module.exports = {
                 codemirror: {
                   name: 'chunk-tuiEditor', // split elementUI into a single package
                   priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?tcodemirror(.*)/ // in order to adapt to cnpm
+                  test: /[\\/]node_modules[\\/]_?codemirror(.*)/ // in order to adapt to cnpm
                 },
                 highlight: {
                   name: 'chunk-highlight', // split elementUI into a single package
